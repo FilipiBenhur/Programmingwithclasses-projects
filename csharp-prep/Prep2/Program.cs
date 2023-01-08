8c5e04a264d0f488a0d7bb6c196f7810b762554a
FilipiBenhur <cru21009@byui.edu>
2023-01-08 01:08:36 +0000

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int grade = int.Parse(percentage);
+
+        string letter = "";
+        if ( grade >= 90 )
+        {
+            letter = "A";
+        }
+        else if ( grade >= 80 )
+        {
+            letter = "B";
+        }
+        else if ( grade >= 70 )
+        {
+            letter = "C";
+        }
+        else if ( grade >= 60 )
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your Grade is {letter}");
+
+
+        if ( grade >= 70 )
+        {
+            Console.WriteLine("Congratulations, you passed the class!");
+        }
+
+        else
+        {
+            Console.WriteLine("Try harder next time!");
+        }
+
     }
 }
